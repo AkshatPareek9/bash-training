3a06cb986bfa80e2d21e89e86cc6face1177636c
Akshat Pareek <62641746+AkshatPareek9@users.noreply.github.com>
2025-11-23 20:56:58 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 
-#chmod +x script.sh
+# chmod +x script.sh
 # ./script.sh
-
 
 username="akshat"
 echo $username
 
-#chmod +x ./script.sh
+# if loop to check existance of file
+if [ -e ./abc.txt ]; then
+  echo "File present"
+else
+  echo "File not present"
+fi
 
+# for loop
+for item in file1 file2 file3; do
+  echo $item
+done
+
+# redirect stdout and stderr in output file
+ls /home/ubuntu/abc.txt &> output.txt
+
+# $? to check last command success result
+ls /root/abc.txt
+echo $?
+# 0 -> success
+# non-zero -> failure
+
+# use set for debug
 set -x
 # Arithmatic operation
 echo $((3+4))
